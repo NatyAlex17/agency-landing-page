f5f0cde3de015342f938a23ad320d99250cdb41a
Natnael Alemayehu <natnael@triaplc.com>
2023-04-21 01:13:38 +0300

    * **** Minor changes     Title , ReadMe changed

--- src/Components/nav.js
+++ src/Components/nav.js
@@ -6,7 +6,7 @@ export function Menu()
         <div className="absolute w-full top-4 align-middle items-center flex flex-row justify-between px-8">
           <p className="py-0 bg-teal-400 rounded-sm basis-1/12 text-center font-bold text-xl"> o o</p>
         <div className='flex text-xs font-josefin  text-gray-600 font-bold  items-end '>
-            <button className=" text-gray-8 00  rounded py-1 px-3" > Home</button>
+            <button className=" text-gray-800  rounded py-1 px-3" > Home</button>
             <button className="  rounded py-1 px-3" > About Us</button>
             <button className="  rounded py-1 px-3" > Services</button>
             <button className="  rounded py-1 px-3" > News</button>
